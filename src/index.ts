10458cf554c373499942a4fc1808b24e3bbffad2
ws325430 <56073707+ws325430@users.noreply.github.com>
2022-05-07 14:12:29 +0100

    Fix for error
    - Error occured due to the file dialog being used in the render process
    - Fixed error by moving file dialog to the main process
    - Added communication between render and main processes with IPC lib from electron

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-//language: typescript
-
-import { app, BrowserWindow, clipboard } from "electron";
+import { app, BrowserWindow, clipboard, dialog} from "electron";
 import * as fs from "original-fs";
-
 import * as path from 'path';
+import {ipcMain} from "electron";
 
 app.on('ready', () => {
     console.log('App is ready');
@@ -23,6 +21,67 @@ app.on('ready', () => {
     }).catch(e => console.error(e));
 });
 
+const pathLocation = __dirname;
+let imagePath = "";
+
+// --- IPC Events ---
+
+ipcMain.on('get-clipboard-image', (event) => {
+    const image = clipboard.readImage();
+    if (image) {
+        // write image to file
+        writeClipboardImageToFile(image);
+        console.log('Image written to file');
+        //update target image file path
+        imagePath = path.join(pathLocation + '/clipboard.png');
+    }
+    else {
+        console.log('No image in clipboard');
+    }
+});
+
+
+ipcMain.on('get-image-path', (event) => {
+    getImagePathFromFileBrowser();
+});
+
+// --- Get Image Functions ---
+
+function getImagePathFromFileBrowser() {
+    const options = {
+        title: 'Select image',
+        properties: ['openFile'],
+        filters: [
+            {name: 'Images', extensions: ['png', 'jpg', 'jpeg', 'gif']}
+        ]
+    }
+
+    dialog.showOpenDialog(<Electron.OpenDialogSyncOptions>options)
+    .then((result: any) => {
+        console.log(result);
+        if (result.canceled) {
+            console.log('Canceled');
+        }
+        else {
+            imagePath = result.filePaths[0];
+            console.log(imagePath);
+        }
+    })
+    .catch((err: any) => {
+        console.log(err);
+    });
+}
+
+
+function writeClipboardImageToFile(image: Electron.NativeImage) {
+    const filePath = path.join(pathLocation + '/clipboard.png');
+    // write file
+    fs.writeFile(filePath, image.toPNG(), (err: any) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
 
 
 
--- src/render.ts
+++ src/render.ts
@@ -1,58 +1,18 @@
-const {dialog, clipboard, nativeImage} = require('electron');
+const {ipcRenderer} = require('electron');
 const fs = require('original-fs');
 const path = require('path');
 
 
-let imagePath = "";
 
 const clipboardButton = document.getElementById('clipboard-image-button');
 const browseButton = document.getElementById('browse-image-button');
 
 clipboardButton?.addEventListener('click', () => {
-    // get image from clipboard
-    const image = getImageFromClipboard();
-    if (image) {
-        // write image to file
-        writeClipboardImageToFile(image);
-        console.log('Image written to file');
-    }
-    else {
-        console.log('No image in clipboard');
-    }
+    ipcRenderer.send('get-clipboard-image');
 });
-
-
 
 
 browseButton?.addEventListener('click', () => {
-    //set imagePath from file browser
-    imagePath = getImagePathFromFileBrowser();
-    console.log('Image path: ' + imagePath);
+    ipcRenderer.send('get-image-path');
 });
-
-
-function getImagePathFromFileBrowser() {
-    const filePath = dialog.showOpenDialogSync({
-        properties: ['openFile'],
-        filters: [
-            { name: 'Images', extensions: ['png', 'jpg', 'jpeg'] }
-        ]
-    });
-    return filePath === undefined ? "" : filePath[0];
-}
-
-
-function getImageFromClipboard() {
-    return clipboard.readImage();
-}
-
-
-function writeClipboardImageToFile(image: Electron.NativeImage) {
-    const filePath = path.join(__dirname + '/clipboard.png');
-    fs.writeFile(filePath, image.toPNG(), (err: any) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-}
 
